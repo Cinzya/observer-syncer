b769cd5aa3f90dbbbfa15aa4cf11f0437f4648e9
Cinzya <cynti-ebert@web.de>
2024-01-26 03:37:58 +0100

    style: add tailwind and form on home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex items-center justify-center h-screen w-full">
+    <div class="w-full max-w-xs">
+        <form class="bg-gray-800 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <div class="mb-4">
+            <label class="block text-white text-sm font-bold mb-2" for="username">
+              Your IP
+            </label>
+            <input class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" id="username" type="text" placeholder="127.0.0.1">
+          </div>
+          <div class="mb-6">
+            <label class="block text-white text-sm font-bold mb-2" for="password">
+              Your Partners IP
+            </label>
+            <input class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" id="password" type="password" placeholder="123.45.78.9">
+          </div>
+          <div class="flex items-center justify-between">
+            <button class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+              Next
+            </button>
+          </div>
+        </form>
+        <!-- <p class="text-center text-gray-500 text-xs">
+          &copy;2020 Acme Corp. All rights reserved.
+        </p> -->
+      </div>
+</div>
